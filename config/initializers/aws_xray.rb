b1270eec4eecd1c2653debdde2274d8b8aff91b1
bhanutejags <bhanutejags@gmail.com>
2021-03-04 21:29:28 -0500

    Initial Commit for AWS Deployment of Treatment Database: Added code to integrate with XRay, 3.

--- config/initializers/aws_xray.rb
+++ config/initializers/aws_xray.rb
@@ -2,5 +2,6 @@ Rails.application.config.xray = {
   name: 'treatment-database',
   patch: %I[net_http aws_sdk],
   # record db transactions as subsegments
-  active_record: true
+  active_record: true,
+  context_missing: 'LOG_ERROR'
 }
