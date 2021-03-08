c5e10f117f22a1ebbd5c6a9bfce7689f570f8be3
bhanutejags <bhanutejags@gmail.com>
2021-03-08 13:30:28 -0500

    Initial Commit for AWS Deployment of Treatment Database: Added code to integrate with XRay, 11, added plugins.

--- config/initializers/aws_xray.rb
+++ config/initializers/aws_xray.rb
@@ -2,6 +2,7 @@
 Rails.application.config.xray = {
   name: 'treatment-database',
   patch: %I[net_http aws_sdk],
+  plugins: %I[ec2 elastic_beanstalk],
   # record db transactions as subsegments
   active_record: true,
   context_missing: 'LOG_ERROR'
